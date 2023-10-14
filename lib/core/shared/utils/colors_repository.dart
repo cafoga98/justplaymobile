7d7817f9f4ddff81b6b5c25b5ecb69da1b7c8500
cforerog <cforerog@intergrupo.com>
2023-10-14 16:06:00 -0500

    Add a new colors repository

---
+++ lib/core/shared/utils/colors_repository.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/animation.dart';
+
+class ColorsRepository {
+  ColorsRepository._();
+
+  ///Principal color
+  static const realBlue = Color(0xFF001F3F);
+
+  static const goldenPoppy = Color(0xFFFFC300);
+  static const platinum = Color(0xFFE5E5E5);
+  static const coralRed = Color(0xFFFF4136);
+  static const limeGreen = Color(0xFF2ECC40);
+  static const white = Color(0xFFFFFFFF);
+}
