66c88ca95597bfde50e7d305adbe1d84ddb00974
cforerog <cforerog@intergrupo.com>
2023-10-14 16:12:40 -0500

    Add login page

--- lib/features/login/presentation/login_page.dart
+++ lib/features/login/presentation/login_page.dart
@@ -1,5 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '/core/shared/utils/colors_repository.dart';
+import '/core/shared/utils/images_reporitory.dart';
+import '/features/login/presentation/widgets/footer_login.dart';
+import '/features/login/presentation/widgets/form_login.dart';
 
 @RoutePage()
 class LoginPage extends StatefulWidget {
@@ -12,6 +18,36 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorsRepository.realBlue,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.w),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 300.h,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        ImageRepository.loginHeader,
+                      ),
+                    ),
+                  ),
+                ),
+                const FormLogin(),
+                const FooterLogin(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
