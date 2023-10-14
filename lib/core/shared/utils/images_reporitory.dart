ab0ca025d9ed4e94e374dcdee90ee662c715c750
cforerog <cforerog@intergrupo.com>
2023-10-14 16:07:16 -0500

    Add image repository file

---
+++ lib/core/shared/utils/images_reporitory.dart
@@ -0,0 +1,19 @@
+class ImageRepository {
+  ImageRepository._();
+  static const loginHeader = 'assets/img/justplay_login.png';
+  static const humansSoccer = 'assets/img/humans-soccer.png';
+  static const humansSkate = 'assets/img/humans-skate.png';
+  static const humansBoxing = 'assets/img/humans-boxing.png';
+  static const humansBicycle = 'assets/img/humans-bicycle.png';
+  static const humansBasketball2 = 'assets/img/humans-basketball2.png';
+  static const humansBasketball = 'assets/img/humans-basketball.png';
+  static const humansScooter = 'assets/img/humans-scooter.png';
+  static const headerCity = 'assets/img/header-city.png';
+  static const gmailIcon = 'assets/img/gmail_icon.png';
+  static const facebookIcon = 'assets/img/facebook_icon.png';
+  static const emailIcon = 'assets/img/email_icon.png';
+  static const amenitiesWifi = 'assets/img/amenities_wifi.png';
+  static const amenitiesParking = 'assets/img/amenities_parking.png';
+  static const amenitiesHydration = 'assets/img/amenities_hydration.png';
+  static const amenitiesFoodStore = 'assets/img/amenities_foodStore.png';
+}
