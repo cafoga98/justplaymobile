935d4f19a1ff8d7a73f7f1bb0f3cecac2a170d54
cforerog <cforerog@intergrupo.com>
2023-10-15 08:19:41 -0500

    Add new widget to show the detail of each player in the competition

---
+++ lib/features/sports/presentation/widgets/players_item.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '/core/shared/utils/style_repository.dart';
+import '/core/shared/utils/colors_repository.dart';
+import '/features/sports/domain/entities/host.dart';
+
+class PlayersItem extends StatelessWidget {
+  final Host player;
+  final int index;
+
+  const PlayersItem({
+    Key? key,
+    required this.player,
+    required this.index,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 5.h, horizontal: 10.w),
+      child: findOrderByIndex(context),
+    );
+  }
+
+  Widget findOrderByIndex(BuildContext context) {
+    if (index.isEven) {
+      //if index is Even
+      return leftWidget(context);
+    } else {
+      //if index is Odd
+      return rightWidget(context);
+    }
+  }
+
+  Widget rightWidget(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 55.h,
+          width: MediaQuery.of(context).size.width / 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                player.name!,
+                maxLines: 2,
+                style: medium.copyWith(
+                  color: ColorsRepository.realBlue,
+                  letterSpacing: 2,
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          width: 20.w,
+        ),
+        Container(
+          height: 55.h,
+          width: 55.h,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: NetworkImage(player.profileImage!),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget leftWidget(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: 55.h,
+          width: 55.h,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: NetworkImage(player.profileImage!),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 20.w,
+        ),
+        SizedBox(
+          height: 55.h,
+          width: MediaQuery.of(context).size.width / 2,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                player.name!,
+                maxLines: 2,
+                style: medium.copyWith(
+                  color: ColorsRepository.realBlue,
+                  letterSpacing: 2,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
